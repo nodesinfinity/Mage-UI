ca9789b7e4537a5bf788da1fe23e0c91e41d162d
anuxr4g <anun20003@gmail.com>
2025-01-26 00:36:58 +0530

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
-    creator: "@mage-ui.design",
+    creator: "Infinity Nodes",
   },
   icons: {
     icon: "/favicon.ico",
